3c6e6ad183a25c2472a9311dc2b57465c805f2b6
marcelosoliveira <msbobsk8@gmail.com>
2021-07-17 15:25:09 -0300

    Atualizando Repositório

--- Projeto-Arquitetura-Microserviços-Springcloud/springcloud-shopping-cart/src/main/java/com/shopping/cart/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shopping.cart.repository;
-
-import com.shopping.cart.model.Cart;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartRepository extends CrudRepository<Cart, Long> {
-}
